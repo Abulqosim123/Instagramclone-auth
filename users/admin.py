a90f2d62ce31e0bbff5efcba0b538156feaad814
Abulqosim123 <rafiqovbulqosim@gmail.com>
2023-11-28 20:42:28 +0500

    Instagramclone

---
+++ users/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import User, UserConfirmation
+
+
+# Register your models here.
+
+
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'phone_number']
+
+
+admin.site.register(User, UserModelAdmin)
+admin.site.register(UserConfirmation)
